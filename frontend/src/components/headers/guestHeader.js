087a6860d10cd4ff1aea5dc4a358592c06bca369
Ali Makkawi <alijmakk@gmail.com>
2020-12-15 17:57:37 +0200

    added customer and guest headers using material UI

--- frontend/src/components/headers/guestHeader.js
+++ frontend/src/components/headers/guestHeader.js
@@ -1,28 +1,123 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import { 
+  AppBar, 
+  Toolbar, 
+  Typography, 
+  makeStyles,
+  Button,
+ } from '@material-ui/core';
 
+const useStyles = makeStyles(() => ({
+  header: {
+     backgroundColor: "#400CCC",
+     paddingRight: "79px",
+     paddingLeft: "118px",
+  },
 
+  logo: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#FFFEFE",
+    textAlign: "left",
+  },
 
-function guestHeader() {
+  menuButton: {
+    fontFamily: "Open Sans, sans-serif",
+    fontWeight: 700,
+    size: "18px",
+    marginLeft: "38px",
+  },
+
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+}));
+
+const guestHeaderData = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Search For Technicians",
+    href: "/search",
+  },
+  {
+    label: "Help",
+    href: "/help",
+  },
+  {
+    label: "Log In",
+    href: "/login",
+  },
+  {
+    label: "Register",
+    href: "/register",
+  },
+];
+
+export default function GuestHeader() {
+  const { header, logo, menuButton, toolbar } = useStyles();
+
+  const displayDesktop = () => {
+    return (
+       <Toolbar className={toolbar}>
+       {TechieFindLogo}
+       <div>{getMenuButtons()}</div>
+       </Toolbar>
+    );
+  };
+  
+  const TechieFindLogo = (
+    <Typography variant="h6" component="h1" className={logo}>
+      TechieFind
+    </Typography> 
+    );
+
+  const getMenuButtons = () => {
+    return guestHeaderData.map(({label, href}) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color:"inherit",
+            to: href,
+            component: RouterLink,
+            className: menuButton
+          }} 
+        >
+        {label}
+        </Button>
+      );
+    });
+  };
+
   return (
-    <nav>
-      <NavLink exact activeClassName="active"  to="/home">
-        Home 
-      </NavLink>
-      <NavLink activeClassName="active"  to="/search">
-        Search For Technicians
-      </NavLink>
-      <NavLink activeClassName="active"  to="/help">
-        Help
-      </NavLink>
-      <NavLink activeClassName="active"  to="/login">
-        Log In
-      </NavLink>
-      <NavLink activeClassName="active"  to="/register">
-        Register
-      </NavLink>
-    </nav>
+    <header>
+      <AppBar className={header}>
+        {displayDesktop()}
+      </AppBar>
+    </header>
   );
-
 }
-export default guestHeader;
+
+{/* <nav>
+<NavLink exact activeClassName="active"  to="/home">
+  Home 
+</NavLink>
+<NavLink activeClassName="active"  to="/search">
+  Search For Technicians
+</NavLink>
+<NavLink activeClassName="active"  to="/help">
+  Help
+</NavLink>
+<NavLink activeClassName="active"  to="/login">
+  Log In
+</NavLink>
+<NavLink activeClassName="active"  to="/register">
+  Register
+</NavLink>
+</nav> */}
