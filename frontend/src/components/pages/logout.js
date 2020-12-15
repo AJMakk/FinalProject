6338a3dab081d450b7e80f08fc2bc6725620b9ae
Ali Makkawi <alijmakk@gmail.com>
2020-12-15 17:59:07 +0200

    added logout

---
+++ frontend/src/components/pages/logout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import api from '../../api';
+import AppContainer from '../containers/AppContainer.js';
+import Button from '@material-ui/core/Button';
+
+
+export default function Logout() {
+  let history = useHistory();
+
+ 
+  const handleLogout = () => {
+    api.logout().then(res => {
+      console.log("res: ",res);
+      
+      localStorage.removeItem('AccessToken');
+      localStorage.removeItem('UsersName');
+      localStorage.clear();
+
+      history.push("/");
+      window.location.reload();
+      alert(res.data.message) ;
+    });
+      
+  }
+
+  return (
+    <AppContainer >
+        <div class="logout-button-container">
+            <Button id="logout-button" type="button" onClick={handleLogout}>
+                logout
+            </Button>
+        </div>
+    </AppContainer>
+  );
+
+}
