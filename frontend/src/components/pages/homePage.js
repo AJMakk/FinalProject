740a4b9041e21ca9e32359a57f76431367b24247
Ali Makkawi <alijmakk@gmail.com>
2020-12-15 17:58:41 +0200

    added home page

---
+++ frontend/src/components/pages/homePage.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import AppContainer from '../containers/AppContainer.js';
+import {Link} from 'react-router-dom';
+
+
+export default function Home() {
+
+   
+return (
+    <AppContainer title="Home">
+        <p>Welcome to the Home Page!</p>
+    </AppContainer>
+)
+    
+};
