411e5d89dacc5d264932fb0a0b31ba35f01228bd
Ali Makkawi <alijmakk@gmail.com>
2020-12-19 01:41:36 +0200

    added backgroud image to theme

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,5 +3,19 @@ const theme = createMuiTheme({
   palette: {
     primary: { 500: '#467fcf' },
   },
+
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          backgroundImage:
+          `url(${process.env.PUBLIC_URL + '/assets/bgimage3.jpg'})`,
+          minHeight: '100vh',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }
+      }
+    }
+  }
 })
 export default theme
