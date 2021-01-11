2ce7e72d0fa67aadc7ab79d1796310ba20263fcb
Ali Makkawi <alijmakk@gmail.com>
2021-01-11 13:31:09 +0200

    added route for categories

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CustomerAuthController;
 use App\Http\Controllers\API\CityController;
+use App\Http\Controllers\API\CategoryController;
+
 
 
 /*
@@ -26,5 +28,7 @@ Route::post('/login', [CustomerAuthController::class,'login']);
 Route::get('/logout', [CustomerAuthController::class,'logout']);
 
 Route::get('/cities', [CityController::class,'index']);
+Route::get('/categories', [CategoryController::class,'index']);
+
 /* Route::apiResource('/cities', CityController::class)->middleware('auth:api');
  */
