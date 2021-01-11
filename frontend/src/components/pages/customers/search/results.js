d2310d97c1a30f2a2cca7a7b4949ac743772334b
Ali Makkawi <alijmakk@gmail.com>
2021-01-11 13:36:23 +0200

    added results for search

--- frontend/src/components/pages/customers/search/results.js
+++ frontend/src/components/pages/customers/search/results.js
@@ -1,107 +1,281 @@
 import React from 'react';
-import cx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import Divider from '@material-ui/core/Divider';
-import { useFadedShadowStyles } from '@mui-treasury/styles/shadow/faded';
-import { useGutterBorderedGridStyles } from '@mui-treasury/styles/grid/gutterBordered';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    marginTop: theme.spacing(10)
-  } ,
-  card: {
-    borderRadius: 12,
-    minWidth: 256,
-    /* textAlign: 'left', */
+function createData(name, speciality, rating, city, distance, schedule) {
+  return { name, speciality, rating, city,distance, schedule };
+}
+
+const rows = [
+  createData('Ali Makkawi', 'Electrician', 5, 'Tripoli', 67, 'link'),
+];
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  { id: 'name', numeric: false, disablePadding: true, label: "Technician's Name" },
+  { id: 'speciality', numeric: false, disablePadding: false, label: 'Speciality' },
+  { id: 'rating', numeric: true, disablePadding: false, label: 'Rating' },
+  { id: 'city', numeric: false, disablePadding: false, label: 'City' },
+  { id: 'distance', numeric: true, disablePadding: false, label: 'Distance' },
+  { id: 'schedule', numeric: false, disablePadding: false, label: 'Check Schedule' },
+];
+
+function EnhancedTableHead(props) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'center' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'default'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
   },
-  avatar: {
-    width: 60,
-    height: 60,
-    margin: 'left',
-  },
-  heading: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    letterSpacing: '0.5px',
-    marginTop: 8,
-    marginBottom: 0,
-    margin:'left',
-  },
-  subheader: {
-    fontSize: 14,
-    color: "grey",
-    marginBottom: '0.875em',
-  },
-  descriptionSubheader: {
-    fontSize: 14,
-    color: "grey",
-    marginBottom: '0.875em',
-    textAlign: 'center',
-  },
-  description: {
-    fontSize: 14,
-    color: "grey",
-    marginBottom: '0.875em',
-    textAlign:'center',
-  },
-  statLabel: {
-    fontSize: 12,
-    color: "grey",
-    fontWeight: 500,
-    fontFamily:
-      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-    margin: 0,
-  },
-  statValue: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 4,
-    letterSpacing: '1px',
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  title: {
+    flex: '1 1 100%',
   },
 }));
 
-export const Results = React.memo(function ProfileCard() {
-  const styles = useStyles();
-  const shadowStyles = useFadedShadowStyles();
-  const borderedGridStyles = useGutterBorderedGridStyles({
-    borderColor: 'rgba(0, 0, 0, 0.08)',
-    height: '50%',
-  });
-  return (
-    <Container className={styles.container}>
-    <Card className={cx(styles.card, shadowStyles.root)}>
-      <CardContent>
-      <Box display={'flex'}>
-        <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
-        <Avatar className={styles.avatar} src={''} />
-        <h3 className={styles.heading}>Ali Makkawi</h3>
-        <span className={styles.subheader}>Electrician</span>
-        </Box>
-        <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
-        <h3 className={styles.descriptionSubheader}>Description</h3>
-        <p className={styles.description}>Electrician with many years experience. worked in all of Lebanon.</p>
-        </Box>
-      </Box>
-      </CardContent>
-      <Divider light />
-      <Box display={'flex'}>
-        <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
-          <p className={styles.statLabel}>Distance</p>
-          <p className={styles.statValue}>5 kms</p>
-        </Box>
-        <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
-          <p className={styles.statLabel}>Rating</p>
-          <p className={styles.statValue}>5</p>
-        </Box>
-      </Box>
-    </Card>
-    </Container>
-  );
-});
+const EnhancedTableToolbar = (props) => {
+  const classes = useToolbarStyles();
 
-export default Results
+  return (
+    
+    <Toolbar
+      className={clsx(classes.root)}
+    >
+        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+          Technicians
+        </Typography>
+      
+        <Tooltip title="Filter list">
+          <IconButton aria-label="filter list">
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      
+    </Toolbar>
+  );
+};
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  paper: {
+    width: '100%',
+    marginTop:theme.spacing(10),
+    marginBottom: theme.spacing(2),
+    background:'none',
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+
+export default function AllTechniciansTable() {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('rating');
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar />
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      tabIndex={-1}
+                      key={row.name}
+                     
+                    >
+                      <TableCell padding="checkbox">
+                        
+                      </TableCell>
+                      <TableCell component="th" id={labelId} scope="row" padding="none">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="left">{row.speciality}</TableCell>
+                      <TableCell align="center">{row.rating}</TableCell>
+                      <TableCell align="left">{row.city}</TableCell>
+                      <TableCell align="center">{row.distance} Kms</TableCell>
+                      <TableCell align="left">{row.schedule}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </div>
+  );
+}
