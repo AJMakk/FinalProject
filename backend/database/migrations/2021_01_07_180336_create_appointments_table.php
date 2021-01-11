33ff835e8121ba19bfe3cd8f5ad0be3fb1797e9a
Ali Makkawi <alijmakk@gmail.com>
2021-01-11 13:30:52 +0200

    added appointments table migration

---
+++ backend/database/migrations/2021_01_07_180336_create_appointments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('startDate');
+            $table->dateTime('endDate');
+            $table->string('location');
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->onDelete('cascade');
+            $table->foreignId('technician_id')
+            ->constrained('technicians')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
