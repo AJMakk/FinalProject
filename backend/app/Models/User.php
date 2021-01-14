424f8e3d1cc046b0b280ff75cbba1494de87d246
Ali Makkawi <alijmakk@gmail.com>
2021-01-14 15:06:25 +0200

    added city relationship

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
     {
     	return $this->hasMany(Appointment::class, 'user_id');
     }
+    public function city()
+    {
+    	return $this->belongsTo(City::class, 'city_id');
+    }
 }
