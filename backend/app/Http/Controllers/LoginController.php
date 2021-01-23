041f62ebf760245f9b912a61f3e264259add4651
Ali Makkawi <alijmakk@gmail.com>
2021-01-23 02:53:28 +0200

    removed redundant code

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -9,33 +9,11 @@ use App\Models\Admin;
 use Hash;
 use Validator;
 use Auth;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\TechnicianResource;
 
 class LoginController extends Controller
 {
-    public function userDashboard()
-    {
-        $users = User::all();
-        $success =  $users;
-
-        return response()->json($success, 200);
-    }
-
-    public function adminDashboard()
-    {
-        $users = Admin::all();
-        $success =  $users;
-
-        return response()->json($success, 200);
-    }
-
-    public function technicianDashboard()
-    {
-        $users = Technician::all();
-        $success =  $users;
-
-        return response()->json($success, 200);
-    }
-
     public function userLogin(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -54,31 +32,6 @@ class LoginController extends Controller
             $user = User::select('users.*')->find(auth()->guard('user')->user()->id);
             $success =  $user;
             $success['token'] =  $user->createToken('MyApp',['user'])->accessToken;
-
-            return response()->json($success, 200);
-        }else{
-            return response()->json(['error' => ['Email and Password are Wrong.']], 200);
-        }
-    }
-
-    public function adminLogin(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
-        if($validator->fails()){
-            return response()->json(['error' => $validator->errors()->all()]);
-        }
-
-        if(auth()->guard('admin')->attempt(['email' => request('email'), 'password' => request('password')])){
-
-            config(['auth.guards.api.provider' => 'admin']);
-
-            $admin = Admin::select('admins.*')->find(auth()->guard('admin')->user()->id);
-            $success =  $admin;
-            $success['token'] =  $admin->createToken('MyApp',['admin'])->accessToken;
 
             return response()->json($success, 200);
         }else{
