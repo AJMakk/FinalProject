b87c4d3d1d1462051d76152aea7989cfbd0d50b4
Ali Makkawi <alijmakk@gmail.com>
2021-01-23 04:04:49 +0200

    changed logo name and fixed href link for logging in

--- frontend/src/components/pages/technicians/register/register.js
+++ frontend/src/components/pages/technicians/register/register.js
@@ -20,7 +20,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="/">
-        TechieFind
+        TradieFind
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -212,7 +212,7 @@ export default function TechnicianRegister() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="login" variant="body2">
+              <Link href="http://localhost:3000/login" variant="body2">
                 Already have an account? Log in !
               </Link>
             </Grid>
