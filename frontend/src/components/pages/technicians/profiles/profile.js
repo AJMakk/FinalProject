83f297b4c5221a6e5b7445126ddbb1657924a30b
Ali Makkawi <alijmakk@gmail.com>
2021-01-23 04:04:04 +0200

    added ratings and fixed styling

--- frontend/src/components/pages/technicians/profiles/profile.js
+++ frontend/src/components/pages/technicians/profiles/profile.js
@@ -3,6 +3,7 @@ import cx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import Rating from '@material-ui/lab/Rating';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
@@ -21,8 +22,8 @@ const useStyles = makeStyles((theme) => ({
     /* textAlign: 'left', */
   },
   avatar: {
-    width: 60,
-    height: 60,
+    width: 90,
+    height: 90,
     margin: 'left',
   },
   heading: {
@@ -48,7 +49,7 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 14,
     color: "grey",
     marginBottom: '0.875em',
-    textAlign:'center',
+    textAlign:'left',
   },
   statLabel: {
     fontSize: 12,
@@ -79,25 +80,27 @@ export const TechnicianProfile = React.memo(function ProfileCard() {
       <CardContent>
       <Box display={'flex'}>
         <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
-        <Avatar className={styles.avatar} src={''} />
-        <h3 className={styles.heading}>Ali Makkawi</h3>
-        <span className={styles.subheader}>Electrician</span>
+        <Avatar className={styles.avatar} src={'/assets/omarKabbara.jpg'} />
+        <h3 className={styles.heading}>Omar Kabbara</h3>
+        <span className={styles.subheader}>Plumber</span>
         </Box>
         <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
         <h3 className={styles.descriptionSubheader}>Description</h3>
-        <p className={styles.description}>Electrician with many years experience. worked in all of Lebanon.</p>
+        <p className={styles.description}>Highly skilled Plumber with 6 years’ experience in both industrial and 
+        commercial plumbing in Lebanon.<br></br> <br></br>Demonstrated track record troubleshooting complex plumbing systems, estimating and 
+        controlling project costs,<br></br> and ensuring compliance with all trade practices and plumbing codes. </p>
         </Box>
       </Box>
       </CardContent>
       <Divider light />
       <Box display={'flex'}>
         <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
-          <p className={styles.statLabel}>Distance</p>
-          <p className={styles.statValue}>5 kms</p>
+          <p className={styles.statLabel}>City</p>
+          <p className={styles.statValue}>Tripoli</p>
         </Box>
         <Box p={2} flex={'auto'} className={borderedGridStyles.item}>
           <p className={styles.statLabel}>Rating</p>
-          <p className={styles.statValue}>5</p>
+          <Rating name="read-only" value={4} readOnly />               
         </Box>
       </Box>
     </Card>
