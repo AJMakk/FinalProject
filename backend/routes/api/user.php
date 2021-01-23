997719e8ec760b6dcfaa06e48d627df949bd4870
Ali Makkawi <alijmakk@gmail.com>
2021-01-23 03:51:33 +0200

    added routes for sending & showing messages , get profile , store appointment request

--- backend/routes/api/user.php
+++ backend/routes/api/user.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AppointmentsController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MessageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +28,9 @@ Route::post('user/login',[LoginController::class, 'userLogin'])->name('userLogin
 Route::group( ['prefix' => 'user','middleware' => ['auth:user-api','scopes:user'] ],function(){
    // authenticated staff routes here
     Route::get('dashboard',[LoginController::class, 'userDashboard']);
+    Route::post('requestappointment',[AppointmentsController::class, 'store']);
+    Route::post('profile',[ProfileController::class, 'userProfile']);
+    Route::post('messages', [MessageController::class, 'store']);
+    Route::get('messages', [MessageController::class, 'index']);
+
 });
