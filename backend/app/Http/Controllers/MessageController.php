9d62cc126e5085fc73c75391c660588e2f3205f8
Ali Makkawi <alijmakk@gmail.com>
2021-01-28 05:12:53 +0200

    added functions to get One to One messages, to get users that have messaged a technician & two functions to store messages for customers and technicians

--- backend/app/Http/Controllers/MessageController.php
+++ backend/app/Http/Controllers/MessageController.php
@@ -5,33 +5,109 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Message;
 use App\Models\User;
-use App\Events\MessageSent;
+use Illuminate\Support\Facades\Auth;
+use App\Events\MessageSentToUser;
+use App\Events\MessageSentToTechnician;
+use App\Http\Resources\MessageResource;
+
 
 class MessageController extends Controller
 {
-    public function __construct()
+
+    public function getOneToOneMessages(Request $request)
     {
-        $this->middleware('auth:user-api');
+
+        $messages = Message::latest()
+        ->where('user_id',$request->user_id)
+        ->where('technician_id',$request->technician_id)
+        ->get();
+
+        /* foreach ($messages as $mg)
+        {
+            $mg->user;
+            $mg->technician;
+
+        }
+        unset($mg); */
+
+        return response([ 'messages' => MessageResource::collection($messages), 'info' => 'Retrieved Successfuly'],200);
     }
 
-    public function index()
+     public function getUsers(Request $request)
     {
-        return Message::with('user')->get();
+
+        $messages = Message::latest()
+        ->where('technician_id',$request->technician_id)
+        ->get();
+        $users = [];
+        foreach ($messages as $mg)
+        {
+            array_push($users, $mg->user);
+
+        }
+        unset($mg);
+
+        $uniqueUsers = array_unique($users, SORT_REGULAR);
+
+        /* foreach($uniqueUsers as $usr)
+        {
+            $usr = User::find($usr->id);
+        }
+        unset($usr);
+
+        foreach($uniqueUsers as $usr)
+        {
+            $usr->messages;
+        }
+ */
+        return response([ 'users' => MessageResource::collection($uniqueUsers), 'info' => 'Retrieved Successfuly'],200);
     }
 
-    public function store(Request $request)
-    {
-        $user = auth()->user();
 
-        $message = $user->messages()->create([
-            'message' => $request->input('message')
+    public function userStore(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'content'=>'required|max:1000',
+            'user_id' => 'required|max:100',
+            'technician_id'=>'required|max:100',
+            'sent_by_user'=>'required|boolean'
         ]);
 
-        broadcast(new MessageSent($message, $user))->toOthers();
+        $message = Message::create($validatedData);
 
-        return [
-            'message' => $message,
-            'user' => $user,
-        ];
+        $message->user;
+        $message->technician;
+
+        /* MessageSentToTechnician::dispatch($message); */
+
+        broadcast(new MessageSentToTechnician($message))->toOthers();
+
+        return response(['message' => new MessageResource($message), 'info' => 'Created successfully'], 200);
     }
+
+    public function technicianStore(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'content'=>'required|max:1000',
+            'user_id' => 'required|max:100',
+            'technician_id'=>'required|max:100',
+            'sent_by_user'=>'required|boolean'
+        ]);
+
+        $message = Message::create($validatedData);
+
+        $message->user;
+        $message->technician;
+
+        broadcast(new MessageSentToUser($message))->toOthers();
+
+
+        return response(['message' => new MessageResource($message), 'info' => 'Created successfully'], 200);
+    }
+
+
+
+
 }
