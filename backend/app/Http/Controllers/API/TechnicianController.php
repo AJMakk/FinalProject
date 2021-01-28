401dab71661555954d93c772050d8cbaaf83c28e
Ali Makkawi <alijmakk@gmail.com>
2021-01-28 05:16:44 +0200

    added search function to allow users to search customers for messaging

--- backend/app/Http/Controllers/API/TechnicianController.php
+++ backend/app/Http/Controllers/API/TechnicianController.php
@@ -41,4 +41,20 @@ class TechnicianController extends Controller
         return response(['technician' => new TechnicianResource($technician), 'message' => 'Retrieved Successfully'], 200);
     }
 
+    public function search(Request $request)
+    {
+
+
+        $names = explode(" ", $request->name);
+
+    // Search each Name Field for any specified Name
+    $technicians = Technician::where(function($query) use ($names) {
+        $query->whereIn('first_name', $names);
+        $query->orWhere(function($query) use ($names) {
+            $query->whereIn('last_name', $names);
+        });
+    })->get();
+
+        return response (['technicians' => TechnicianResource::collection($technicians), 'message' => 'Retrieved successfully'], 200);
+    }
 }
