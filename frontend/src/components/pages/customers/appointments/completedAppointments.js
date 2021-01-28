93c1ec1f914fd2ed220395751e862687840b8e68
Ali Makkawi <alijmakk@gmail.com>
2021-01-28 06:00:48 +0200

    removed redundant imports

--- frontend/src/components/pages/customers/appointments/completedAppointments.js
+++ frontend/src/components/pages/customers/appointments/completedAppointments.js
@@ -1,13 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useHistory, useParams} from 'react-router-dom';
 import api from '../../../../api';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import { lighten, makeStyles } from '@material-ui/core/styles';
