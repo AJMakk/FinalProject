7e099ca74e112d16adfdabe5355e497b5f908636
Ali Makkawi <alijmakk@gmail.com>
2021-01-28 05:19:02 +0200

    added routes for deleting and editing an appointment & for sending and getting on to one messages

--- backend/routes/api/user.php
+++ backend/routes/api/user.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AppointmentsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\MessageController;
+use App\http\Controllers\API\CustomerAuthController;
 
 
 /*
@@ -18,19 +19,27 @@ use App\Http\Controllers\MessageController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('user/register', [CustomerAuthController::class,'register']);
 
 Route::post('user/appointments/create',[AppointmentsController::class, 'store']);
-
 Route::post('user/appointments',[AppointmentsController::class, 'index']);
 
 
 Route::post('user/login',[LoginController::class, 'userLogin'])->name('userLogin');
+
 Route::group( ['prefix' => 'user','middleware' => ['auth:user-api','scopes:user'] ],function(){
    // authenticated staff routes here
-    Route::get('dashboard',[LoginController::class, 'userDashboard']);
     Route::post('requestappointment',[AppointmentsController::class, 'store']);
+    Route::post('deleteappointment',[AppointmentsController::class, 'destroy']);
+    Route::put('editappointment',[AppointmentsController::class, 'update']);
+    Route::post('previousappointments',[AppointmentsController::class, 'previousAppointments']);
+
+
+
     Route::post('profile',[ProfileController::class, 'userProfile']);
-    Route::post('messages', [MessageController::class, 'store']);
-    Route::get('messages', [MessageController::class, 'index']);
+
+    Route::post('sendmessage', [MessageController::class, 'userStore']);
+    Route::post('allmessages', [MessageController::class, 'getOneToOneMessages']);
+
 
 });
