fa1521f3c1982da3d7bc053256dc6bedc25f996c
Ali Makkawi <alijmakk@gmail.com>
2021-02-24 00:12:09 +0200

    added rated attribute to appointment model

--- backend/app/Models/Appointment.php
+++ backend/app/Models/Appointment.php
@@ -10,7 +10,7 @@ class Appointment extends Model
     use HasFactory;
 
     protected $fillable = [
-        'title','startDate','endDate','location','user_id','technician_id','approved'
+        'title','startDate','endDate','location','user_id','technician_id','approved','rated'
     ];
 
     protected $hidden = ['timestamp'];
