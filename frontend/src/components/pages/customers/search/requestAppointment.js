f24638a8257b7cd9dba50d29c457e8bcea6a5cd5
Ali Makkawi <alijmakk@gmail.com>
2021-02-24 00:19:58 +0200

    removed redundant code and addred rated attribute for requesting appointment

--- frontend/src/components/pages/customers/search/requestAppointment.js
+++ frontend/src/components/pages/customers/search/requestAppointment.js
@@ -4,8 +4,6 @@ import api from '../../../../api';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -61,7 +59,7 @@ export default function TechnicianRegister() {
     const startDate = date + ' ' + startTime;
     const endDate = date + ' ' + endTime;
     const approved = 0;
-    ;
+    const rated = 0;
     
 
     console.log('startDate: ', startDate);
@@ -73,7 +71,7 @@ export default function TechnicianRegister() {
 
     try {
      await api.requestAppointment({
-          title, startDate, endDate, location, userId, techId, approved
+          title, startDate, endDate, location, userId, techId, approved, rated
      });
      history.push('/')
     }
